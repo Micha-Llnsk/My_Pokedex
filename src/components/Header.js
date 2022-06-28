f51c7b51a1f80c6cb997f5f1063de3918e504ddf
MichaLlnsk <michael.lulinski@gmail.com>
2022-06-29 00:46:52 +0200

    Fix/search to submit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,26 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
 import {useHistory} from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function Header() {
-
+    const [search, setSearch] = useState("");
     const history = useHistory();
 
+    useEffect(() => {
+        history.push(`/pokemon/${search}`);
+    }, [search, history]);
+
     function handleSubmit(e) {
-        history.push(`/pokemon/${e.target.value}`);
         e.preventDefault();
+        setSearch(e.target[0].value);
     }
 
     return (
         <header>
             <nav className="header">
                 <NavLink className="header__home" to="/">Pokédex</NavLink>
-                <form onChange={handleSubmit} className="header__right">
+                <form onSubmit={handleSubmit} className="header__right">
                     <input 
                         className="header__input" 
                         name="search"
@@ -23,7 +28,7 @@ export default function Header() {
                         type="search"
                         placeholder="Pokémon Name or Nr." 
                     />
-                    <button className="header__button" type="submit" onClick={handleSubmit}>GO</button>
+                    <button className="header__button" type="submit">GO</button>
                 </form>
             </nav>
         </header>
